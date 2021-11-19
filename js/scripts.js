d46afd0455fe354668ac467cbc922de3af9e11dc
NathanStanding <standingtall1000@gmail.com>
2021-11-19 08:05:53 -0700

    Week 4 Assignment

--- js/scripts.js
+++ js/scripts.js
@@ -16,3 +16,12 @@ $(function(){
         }  
     });
 });
+
+$(function(){
+    $("#reserveButton").click(function(){
+        $("#reserveModal").modal("show");
+    });
+    $("#loginButton").click(function(){
+        $("#loginModal").modal("show");
+    });
+});
